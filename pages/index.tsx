12aeaf31baaa1027bf3004420bedd12a86c1df45
12yearoldwithacreditcard <90388519+12yearoldwithacreditcard@users.noreply.github.com>
2021-09-11 12:14:49 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ interface Props {
 const Robe = ({ robe }: { robe: RobeInfo }) => {
   return (
     <a href={robe.url} target="_blank">
-      <div className="m-auto pb-4 mb-8 flex flex-col justify-center items-center gap-2 p-4 md:m-4 border border-white transform hover:scale-105 transition-all bg-blue w-full md:w-96">
+      <div className="m-auto pb-4 mb-8 flex flex-col justify-center items-center gap-2 p-4 md:m-4 border border-white transform hover:scale-105 transition-all bg-black w-full md:w-96">
         <img src={robe.svg} alt="" width="350" height="350" />
         <div className="text-center">
           <p className="text-lg">#{robe.id}</p>
