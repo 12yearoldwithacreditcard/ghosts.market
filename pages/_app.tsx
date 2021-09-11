04733223179f12fe086ef32bfb661e839c234520
12yearoldwithacreditcard <90388519+12yearoldwithacreditcard@users.noreply.github.com>
2021-09-11 12:29:10 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function Robes({ Component, pageProps }) {
       <style jsx global>
         {`
           body {
-            background: #00004d;
+            background: #000033;
             color: white;
             overflow-x: hidden;
           }
